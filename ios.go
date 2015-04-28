ddd1490c2a757b42577f3930cca24e6c3238d2ac
Fan Yang <missdeer@dfordsoft.com>
2015-04-28 17:37:21 +0800

    (*)wip

--- ios.go
+++ ios.go
@@ -13,25 +13,85 @@ import (
 	"strings"
 )
 
+type handler func(image.Image, string, *LaunchImageSpec) error
+
 type LaunchImageSpec struct {
 	Width   int
 	Height  int
 	Postfix string
+	Handler handler
 }
 
 var (
 	LaunchImageSpecifications = []LaunchImageSpec{
-		{320, 480, "~iphone.png"},
-		{640, 960, "@2x~iphone.png"},
-		{640, 1136, "-568h@2x~iphone.png"},
-		{750, 1334, "-667h@2x~iphone.png"},
-		{1242, 2208, "-736h@3x~iphone.png"},
-		{768, 1024, "-Portrait~ipad.png"},
-		{1024, 768, "-Landscape~ipad.png"},
-		{1536, 2048, "-Portrait@2x~ipad.png"},
-		{2048, 1536, "-Landscape@2x~ipad.png"},
+		{320, 480, "~iphone.png", ScaleHandler},
+		{640, 960, "@2x~iphone.png", ScaleHandler},
+		{640, 1136, "-568h@2x~iphone.png", ScaleCutHandler},
+		{750, 1334, "-667h@2x~iphone.png", ScaleCutHandler},
+		{1242, 2208, "-736h@3x~iphone.png", ScaleCutHandler},
+		{768, 1024, "-Portrait~ipad.png", ScaleHandler},
+		{1024, 768, "-Landscape~ipad.png", ScaleCutHandler},
+		{1536, 2048, "-Portrait@2x~ipad.png", SkipHandler},
+		{2048, 1536, "-Landscape@2x~ipad.png", ScaleCutHandler},
 	}
 )
+
+func ScaleHandler(m image.Image, savePath string, spec *LaunchImageSpec) error {
+	bounds := m.Bounds()
+	var im image.Image
+
+	if bounds.Size().Y*spec.Width/bounds.Size().X < spec.Height {
+		im = resize.Resize(uint(spec.Width), 0, m, resize.Bilinear)
+		log.Println("resize by width")
+	} else {
+		im = resize.Resize(0, uint(spec.Height), m, resize.Bilinear)
+		log.Println("resize by height")
+	}
+
+	if err := saveImage(&im, savePath, 1); err != nil {
+		log.Println(savePath, err)
+	}
+	return nil
+}
+
+func ScaleCutHandler(m image.Image, savePath string, spec *LaunchImageSpec) error {
+	bounds := m.Bounds()
+	var im image.Image
+
+	if bounds.Size().Y*spec.Width/bounds.Size().X < spec.Height {
+		im = resize.Resize(uint(spec.Width), 0, m, resize.Bilinear)
+		log.Println("resize by width")
+	} else {
+		im = resize.Resize(0, uint(spec.Height), m, resize.Bilinear)
+		log.Println("resize by height")
+	}
+
+	// if the origin is larger than expected
+	var err error
+	im, err = cutter.Crop(im, cutter.Config{
+		Width:  spec.Width,
+		Height: spec.Height,
+		Anchor: image.Point{(m.Bounds().Size().X - spec.Width) / 2, (m.Bounds().Size().Y - spec.Height) / 2},
+	})
+	log.Printf("cropped to %d * %d\n", im.Bounds().Size().X, im.Bounds().Size().Y)
+
+	if err != nil {
+		log.Println(savePath, err)
+		return err
+	}
+	if err := saveImage(&im, savePath, 1); err != nil {
+		log.Println(savePath, err)
+		return err
+	}
+	return nil
+}
+
+func SkipHandler(m image.Image, savePath string, spec *LaunchImageSpec) error {
+	if err := saveImage(&m, savePath, 1); err != nil {
+		log.Println(savePath, err)
+	}
+	return nil
+}
 
 func GenerateLaunchImage(origin string) error {
 	reader, err := os.Open(origin)
@@ -45,7 +105,6 @@ func GenerateLaunchImage(origin string) error {
 		log.Println(origin, err)
 		return err
 	}
-	bounds := m.Bounds()
 
 	base := filepath.Base(origin)
 	base = base[:strings.Index(base, ".")]
@@ -54,58 +113,7 @@ func GenerateLaunchImage(origin string) error {
 		savePath := base + spec.Postfix
 
 		log.Println("generating ", savePath)
-		if cut == true {
-			// if the origin is smaller than expected
-			if bounds.Size().X >= spec.Width && bounds.Size().Y < spec.Height {
-				// scale first, then cut
-				m = resize.Resize(0, uint(spec.Height), m, resize.Bilinear)
-				log.Printf("first resized to %d * %d\n", m.Bounds().Size().X, m.Bounds().Size().Y)
-				goto do_cut
-			}
-
-			if bounds.Size().X < spec.Width && bounds.Size().Y >= spec.Height {
-				// scale first, then cut
-				m = resize.Resize(uint(spec.Width), 0, m, resize.Bilinear)
-				log.Printf("or resized to %d * %d\n", m.Bounds().Size().X, m.Bounds().Size().Y)
-				goto do_cut
-			}
-
-			if bounds.Size().X < spec.Width && bounds.Size().Y < spec.Height {
-				// fall through, just scale
-				goto do_scale
-			}
-		do_cut:
-			// if the origin is larger than expected
-			im, err := cutter.Crop(m, cutter.Config{
-				Width:  spec.Width,
-				Height: spec.Height,
-				Anchor: image.Point{(m.Bounds().Size().X - spec.Width) / 2, (m.Bounds().Size().Y - spec.Height) / 2},
-			})
-			log.Printf("cropped to %d * %d\n", im.Bounds().Size().X, im.Bounds().Size().Y)
-
-			if err != nil {
-				log.Println(savePath, err)
-				continue
-			}
-
-			if err := saveImage(&im, savePath, 1); err != nil {
-				log.Println(savePath, err)
-			}
-			continue
-		}
-	do_scale:
-		// scale it
-		var im image.Image
-		if bounds.Size().X > spec.Width &&
-			bounds.Size().Y*spec.Width/bounds.Size().X < spec.Height {
-			im = resize.Resize(uint(spec.Width), 0, m, resize.Bilinear)
-		} else {
-			im = resize.Resize(0, uint(spec.Height), m, resize.Bilinear)
-		}
-		log.Printf("finally, resized to %d * %d\n", im.Bounds().Size().X, im.Bounds().Size().Y)
-		if err := saveImage(&im, savePath, 1); err != nil {
-			log.Println(savePath, err)
-		}
+		spec.Handler(m, savePath, &spec)
 	}
 	return nil
 }
